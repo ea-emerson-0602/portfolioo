d01623d445ceead854c5fc1605a5a2490ffc87ea
ea-emerson-0602 <aghoghoemerson7@gmail.com>
2024-08-22 13:31:49 +0100

    made few ui changes, and included links to docs for skills

--- app/(site)/components/Skills.tsx
+++ app/(site)/components/Skills.tsx
@@ -1,45 +1,25 @@
 import { getSkills } from "@/sanity/sanity.query";
 import { SkillType } from "@/types";
 import Image from "next/image";
-import Link from "next/link";
 
 export default async function Skills() {
   const skills: SkillType[] = await getSkills();
 
   return (
-    <div>
-      <div className="hidden md:block">
-      <h1>
-        My Skills
-      </h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-8 lg:gap-10">
-        {skills.map((skill) => (
-          <div
-            key={skill.name}
-            className="flex cursor-pointer flex-col items-center justify-center py-10 bg-skills-grey hover:bg-primary-yellow rounded-3xl"
-          >
-            <Image
-              src={skill.logo}
-              width={100}
-              height={100}
-              alt={skill.name}
-              className="rounded-lg transform transition-transform duration-300 hover:scale-110"
-            />
-            <div className="text-xl font-semibold mb-4 text-center">
-              {skill.name}
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <div className="flex flex-col items-center">
+      <h1 className="md:block hidden text-center my-4">My Skills</h1>
+      <h1 className="block md:hidden font-bold justify-center text-4xl py-4">My Skills</h1>
 
-    <div className="block md:hidden px-6 py-10">
-    <span className="font-bold justify-center flex text-4xl h1 pb-4">My Skills</span>
-      <div className="grid grid-cols-2 gap-6 ">
+      {/* Skills View */}
+      <div className="flex flex-wrap justify-center gap-6 w-full">
         {skills.map((skill) => (
-          <Link href={skill.skillDocUrl!=null ? skill.skillDocUrl : "/"} target="blank"
+          <a
             key={skill.name}
-            className="flex cursor-pointer flex-col items-center justify-center py-4 bg-skills-grey hover:bg-primary-yellow rounded-3xl"
+            href={skill.skillDocUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex cursor-pointer flex-col items-center justify-center py-4 md:py-6 lg:py-8 bg-skills-grey hover:bg-primary-yellow rounded-3xl
+            w-[calc(50%-1rem)] sm:w-[calc(50%-1rem)] md:w-[calc(33.333%-2rem)] lg:w-[calc(25%-2.5rem)]"
           >
             <Image
               src={skill.logo}
@@ -48,13 +28,12 @@ export default async function Skills() {
               alt={skill.name}
               className="rounded-lg transform transition-transform duration-300 hover:scale-110"
             />
-            <div className="text-xl font-semibold mb-4 text-center">
+            <div className="text-lg font-semibold mb-4 text-center">
               {skill.name}
             </div>
-          </Link>
+          </a>
         ))}
       </div>
-    </div>
     </div>
   );
 }
--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -12,70 +12,122 @@ export default function ContactSection() {
   return (
     <div>
       <main className="md:block hidden mx-auto h-fit bg-skills-grey  lg:mt-32 md:mt-24">
-  <div className="flex flex-col items-center w-full">
+        <div className="flex flex-col items-center w-full">
+          {/* Top Border */}
+          <div className="border-t-3 border-primary-yellow w-full"></div>
+          <div className="flex items-center justify-center w-full py-8">
+            <h1 className="px-4 ">Contact Me</h1>
+          </div>
 
-    {/* Top Border */}
-    <div className="border-t-3 border-primary-yellow w-full"></div>
-      <div className="flex items-center justify-center w-full py-8">
-        <h1 className="px-4 ">Contact Me</h1>
-      </div>
+          {/* Contact Information */}
+          <div className="flex flex-col items-center gap-8 my-8">
+            <div className="flex justify-center gap-8">
+              <ContactItem
+                icon={<FaEnvelope />}
+                text="aghoghoemerson7@email.com"
+              />
+              <ContactItem icon={<FaPhoneAlt />} text="(234) 810-581-9021" />
+            </div>
+            <div className="flex justify-center">
+              <ContactItem
+                icon={<FaMapMarkerAlt />}
+                text="Benin City, Nigeria"
+              />
+            </div>
+          </div>
 
-      {/* Contact Information */}
-      <div className="flex flex-col items-center gap-8 my-8">
-        <div className="flex justify-center gap-8">
-          <ContactItem icon={<FaEnvelope />} text="aghoghoemerson7@email.com" />
-          <ContactItem icon={<FaPhoneAlt />} text="(234) 810-581-9021" />
+          {/* Social Icons Section */}
+          <div className="flex items-center justify-center w-full py-8">
+            <div className="flex justify-center gap-6 text-primary-yellow px-4 z-10">
+                
+                  <SocialIcon
+                  target="_blank"
+                rel="noopener noreferrer"
+               href="https://facebook.com/EmersonAghogho"
+                icon={<FaFacebook />}
+              />
+              
+              <SocialIcon
+                href="https://x.com/emerrson7"
+                target="_blank"
+                rel="noopener noreferrer"
+                icon={<FaTwitter />}
+              />
+              <SocialIcon
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                icon={<FaLinkedin />}
+              />
+              <SocialIcon
+                href="https://wa.me/2348105819021"
+                target="_blank"
+                rel="noopener noreferrer"
+                icon={<FaWhatsapp />}
+              />
+            </div>
+          </div>
         </div>
-        <div className="flex justify-center">
-          <ContactItem icon={<FaMapMarkerAlt />} text="Benin City, Nigeria" />
+
+        {/* Bottom Border */}
+        <div className="border-t-3 border-primary-yellow w-full"></div>
+      </main>
+
+      <main className="block md:hidden mx-auto h-fit bg-skills-grey mt-10">
+        <div className="flex flex-col items-center w-full">
+          {/* Top Border */}
+          <div className="border-t-3 border-primary-yellow w-full"></div>
+          <div className="flex items-center justify-center w-full py-4">
+            <span className="font-bold justify-center w-fit mx-auto flex text-4xl h1 border-b-3 border-b-primary-yellow">
+              Contact Me
+            </span>
+          </div>
+
+          {/* Contact Information */}
+          <div className="flex  flex-col gap-8 my-4">
+            <ContactItem
+              icon={<FaEnvelope />}
+              text="aghoghoemerson7@email.com"
+            />
+            <ContactItem icon={<FaPhoneAlt />} text="(234) 810-581-9021" />
+            <ContactItem icon={<FaMapMarkerAlt />} text="Benin City, Nigeria" />
+          </div>
+
+          {/* Social Icons Section */}
+          <div className="flex items-center justify-center w-full py-4">
+          <div className="flex justify-center gap-4 text-primary-yellow px-4 z-10">
+          <SocialIcon
+                  target="_blank"
+                rel="noopener noreferrer"
+               href="https://facebook.com/EmersonAghogho"
+                icon={<FaFacebook />}
+              />
+              
+              <SocialIcon
+                href="https://x.com/emerrson7"
+                target="_blank"
+                rel="noopener noreferrer"
+                icon={<FaTwitter />}
+              />
+              <SocialIcon
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                icon={<FaLinkedin />}
+              />
+              <SocialIcon
+                href="https://wa.me/2348105819021"
+                target="_blank"
+                rel="noopener noreferrer"
+                icon={<FaWhatsapp />}
+              />
+          </div>
+          </div>
         </div>
-      </div>
 
-      {/* Social Icons Section */}
-      <div className="flex items-center justify-center w-full py-8">
-        <div className="flex justify-center gap-6 text-primary-yellow px-4 z-10">
-          <SocialIcon href="#" icon={<FaFacebook />} />
-          <SocialIcon href="#" icon={<FaTwitter />} />
-          <SocialIcon href="#" icon={<FaLinkedin />} />
-          <SocialIcon href="#" icon={<FaWhatsapp />} />
-        </div>
-      </div>
-    </div>
-
-    {/* Bottom Border */}
-    <div className="border-t-3 border-primary-yellow w-full"></div>
-</main>
-
-<main className="block md:hidden mx-auto h-fit bg-skills-grey mt-10">
-  <div className="flex flex-col items-center w-full">
-
-    {/* Top Border */}
-    <div className="border-t-3 border-primary-yellow w-full"></div>
-      <div className="flex items-center justify-center w-full py-4">
-        <span className="font-bold justify-center w-fit mx-auto flex text-4xl h1 border-b-3 border-b-primary-yellow">Contact Me</span>
-      </div>
-
-      {/* Contact Information */}
-      <div className="flex  flex-col gap-8 my-4">
-          <ContactItem icon={<FaEnvelope />} text="aghoghoemerson7@email.com" />
-          <ContactItem icon={<FaPhoneAlt />} text="(234) 810-581-9021" />
-          <ContactItem icon={<FaMapMarkerAlt />} text="Benin City, Nigeria" />
-      </div>
-
-      {/* Social Icons Section */}
-      <div className="flex items-center justify-center w-full py-4">
-        <div className="flex justify-center gap-4 text-primary-yellow px-4 z-10">
-          <SocialIcon href="#" icon={<FaFacebook />} />
-          <SocialIcon href="#" icon={<FaTwitter />} />
-          <SocialIcon href="#" icon={<FaLinkedin />} />
-          <SocialIcon href="#" icon={<FaWhatsapp />} />
-        </div>
-      </div>
-    </div>
-
-    {/* Bottom Border */}
-    <div className="border-t-3 border-primary-yellow w-full"></div>
-</main>
+        {/* Bottom Border */}
+        <div className="border-t-3 border-primary-yellow w-full"></div>
+      </main>
     </div>
   );
 }
@@ -89,10 +141,12 @@ function ContactItem({ icon, text }: { icon: React.ReactNode; text: string }) {
   );
 }
 
-function SocialIcon({ href, icon }: { href: string; icon: React.ReactNode }) {
+function SocialIcon({ href, icon, rel, target }: { href: string; icon: React.ReactNode; rel:string; target:string }) {
   return (
     <a
       href={href}
+      rel={rel}
+      target={target}
       className="text-2xl hover:text-white transition duration-300"
     >
       {icon}
